6d51215725123e7cfb162fc0995e6a0b9a2a2fb0
Tran Nam <tpnam1w3@gmail.com>
2024-10-20 02:00:36 +0700

    cap nhat cac thu muc, set up project backend

---
+++ content-sharing-backend/csapp/src/test/java/com/app/csapp/CsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.csapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
