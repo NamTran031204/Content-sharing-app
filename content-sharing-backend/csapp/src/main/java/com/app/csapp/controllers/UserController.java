4050fb1de0a90fa8f2352dfbd2e8961515d7e036
Tran Nam <tpnam1w3@gmail.com>
2024-10-20 21:08:15 +0700

    cap nhat package controllers va dtos, tao class UserDTO

---
+++ content-sharing-backend/csapp/src/main/java/com/app/csapp/controllers/UserController.java
@@ -0,0 +1,10 @@
+package com.app.csapp.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("api/v1/users")
+public class UserController {
+
+}
---
+++ content-sharing-backend/csapp/src/main/java/com/app/csapp/dtos/UserDTO.java
@@ -0,0 +1,38 @@
+package com.app.csapp.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.Date;
+
+@Builder
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    @NotBlank(message = "username cannot be blank")
+    private String name;
+
+    @NotBlank(message = "email cannot be blank")
+    private String email;
+
+    @NotBlank(message = "username cannot be blank")
+    @JsonProperty("username")
+    private String userName;
+
+    @NotBlank(message = "ban can nhap mat khau")
+    private String password;
+
+    @NotBlank(message = "ban can nhap lai mat khau")
+    private String retypePassword;
+
+    @JsonProperty("profile_picture")
+    private String profilePicture;
+
+    @JsonProperty("created_at")
+    private Date createdAt;
+}
